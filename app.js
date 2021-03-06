3c53ddc8540d7fe77b3f2bdc0454c8890ad463c3
dimittriskalp <dimitriskalp@hotmail.gr>
2021-03-06 18:26:29 +0200

    Some improvements

--- app.js
+++ app.js
@@ -10,8 +10,44 @@ class Book {
 // UI Constructor
 function UI(){
     UI.prototype.addBookToList = function(book){
-        console.log(book);
+      const list = document.getElementById('book-list');
+      const row = document.createElement('tr');
+      
+      // insert cols
+      row.innerHTML = `<td>${book.title}</td>
+                       <td>${book.author}</td>
+                       <td>${book.isbn}</td>
+                       <td><a href="#" class="delete">X</a></td>`;
+      list.appendChild(row);
+
     }
+}
+//Shoe allert
+
+UI.prototype.showAlert = function(message, className){
+    //Create div 
+    const div = document.createElement('div');
+    // Add Classes
+    div.className = `alert ${className}`;
+    //Add  Name
+    div.appendChild(document.createTextNode(message));
+    //Get Parent 
+    const container = document.querySelector('.container');
+    //Get form
+    const form = document.querySelector('#book-form');
+    //Insert alert 
+    container.insertBefore(div, form);
+    //Timeout Alert
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+//Clear fields function
+
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
 }
 
 //Event Listeners
@@ -27,7 +63,16 @@ document.getElementById('book-form').addEventListener('submit',
     // δημιουργω Ui Object
     const ui = new UI();
 
-    ui.addBookToList(book);
+    // Validate 
+    if(title === '' || author === '' || isbn === ''){
+        ui.showAlert('Please fill in all fields', 'error');
+    }else{
+        ui.addBookToList(book);
+        //Show success
+        ui.showAlert('Book Added!', 'success');
+        //Clearfields
+        ui.clearFields();
+    }
 
 
     e.preventDefault();
